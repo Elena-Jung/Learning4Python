58fc284d2707c9efac53a5f9bf6e98932bb7b80b
Elena Jung <kimkm030@gmail.com>
2022-02-28 19:37:20 +0900

    백준 9498 해결

---
+++ B9498.py
@@ -0,0 +1,11 @@
+X = int(input())
+if X >= 90:
+    print("A")
+elif X >= 80 and X < 90:
+    print("B")
+elif X >= 70 and X < 80:
+    print("C")
+elif X >= 60 and X < 70:
+    print("D")
+else:
+    print("F")
